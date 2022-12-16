c4fda00c12439322c12a9351cbb88d13371ad06b
FlynnSC <flynn.calcutt@hotmail.com>
2022-12-16 16:27:21 +1100

    Hacked UI to allows map creation for zk hunt

--- src/components/CreatePage/CenterRegion/Canvas.tsx
+++ src/components/CreatePage/CenterRegion/Canvas.tsx
@@ -5,11 +5,14 @@ import {creationState} from '../../../state/creationState';
 import {observer} from 'mobx-react-lite';
 import {action} from 'mobx';
 import {styling} from '../../styling';
+import {IntegerInput} from '../RightPanel/IntegerInput';
 
 export const CANVAS_LENGTH = 16;
+const ZERO = BigInt(0);
 const ONE = BigInt(1);
 
 export const Canvas = observer(() => {
+  const [canvasSize, setCanvasSize] = useState(16);
   const [painting, setPainting] = useState(false);
 
   const onStartPainting = () => {
@@ -32,53 +35,82 @@ export const Canvas = observer(() => {
     return () => document.removeEventListener('mouseup', listener);
   }, [painting]);
 
+  const updateCanvasSize = (delta: number) => setCanvasSize(canvasSize + delta);
+
+  const logValue = () => {
+    const mask253 = (BigInt(1) << BigInt(253)) - ONE;
+    let value = creationState.tokenId;
+    let str = "[\n";
+    while (true) {
+      str += `'0x${(value & mask253).toString(16)}'`;
+      value >>= BigInt(253);
+      if (value === ZERO) {
+        str += '\n]';
+        break;
+      } else {
+        str += ',\n';
+      }
+    }
+    console.log(str);
+  };
+
   return (
-    <Container onContextMenu={e => e.preventDefault()}>
-      <Grid>
-        {produce(CANVAS_LENGTH * CANVAS_LENGTH, bitIndex => {
-          const shift = BigInt(bitIndex);
-          const isForeground = !!(creationState.tokenId & (ONE << shift));
+    <>
+      <Row>
+        <IntegerInput value={canvasSize} min={16} max={100} onChange={updateCanvasSize}/>
+        <button onClick={logValue}>LOG</button>
+      </Row>
+      <Container onContextMenu={e => e.preventDefault()}>
+        <Grid canvasSize={canvasSize}>
+          {produce(canvasSize * canvasSize, bitIndex => {
+            const shift = BigInt(bitIndex);
+            const isForeground = !!(creationState.tokenId & (ONE << shift));
 
-          const onInput: MouseEventHandler<HTMLDivElement> = action(e => {
-            const mask = ONE << shift;
-            if (e.buttons === 1) {
-              creationState.tokenId |= mask;
-            } else if (e.buttons === 2) {
-              creationState.tokenId &= ~mask;
-            }
-          });
+            const onInput: MouseEventHandler<HTMLDivElement> = action(e => {
+              const mask = ONE << shift;
+              if (e.buttons === 1) {
+                creationState.tokenId |= mask;
+              } else if (e.buttons === 2) {
+                creationState.tokenId &= ~mask;
+              }
+            });
 
-          const onMouseDown: MouseEventHandler<HTMLDivElement> = e => {
-            e.preventDefault();
-            (document.activeElement as HTMLElement)?.blur();
-            onStartPainting();
-            onInput(e);
-          };
+            const onMouseDown: MouseEventHandler<HTMLDivElement> = e => {
+              e.preventDefault();
+              (document.activeElement as HTMLElement)?.blur();
+              onStartPainting();
+              onInput(e);
+            };
 
-          return (
-            <Pixel
-              key={bitIndex}
-              style={{backgroundColor: isForeground ? creationState.foregroundColor : creationState.backgroundColor}}
-              draggable={false}
-              onMouseDown={onMouseDown}
-              onMouseEnter={painting ? onInput : undefined}
-            >
-              <HighlightOverlay/>
-            </Pixel>
-          );
-        })}
-      </Grid>
-    </Container>
+            return (
+              <Pixel
+                key={bitIndex}
+                style={{backgroundColor: isForeground ? creationState.foregroundColor : creationState.backgroundColor}}
+                draggable={false}
+                onMouseDown={onMouseDown}
+                onMouseEnter={painting ? onInput : undefined}
+              >
+                <HighlightOverlay/>
+              </Pixel>
+            );
+          })}
+        </Grid>
+      </Container>
+    </>
   );
 });
+
+const Row = styled.div`
+  display: flex;
+`;
 
 const Container = styled.div`
   width: 100%;
 `;
 
-const Grid = styled.div`
+const Grid = styled.div<{canvasSize: number}>`
   display: grid;
-  grid-template-columns: repeat(${CANVAS_LENGTH}, 1fr);
+  grid-template-columns: repeat(${props => props.canvasSize}, 1fr);
   aspect-ratio: 1/1;
   ${styling.border};
   border-top-width: 0;
--- src/components/CreatePage/CenterRegion/TokenIdInput.tsx
+++ src/components/CreatePage/CenterRegion/TokenIdInput.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import {creationState} from '../../../state/creationState';
-import {ethers} from 'ethers';
 import {action} from 'mobx';
 import {observer} from 'mobx-react-lite';
 import {TextInput} from '../../common/TextInput';
 import {styling} from '../../styling';
 
-const tokenIdToString = (tokenId: bigint) => (
-  ethers.utils.hexZeroPad(`0x${tokenId.toString(16)}`, 32)
-);
-
 export const TokenIdInput = observer(() => {
   const onChange = action((newValueText: string) => {
     try {
-      if (!newValueText.startsWith('0x')) {
-        newValueText = `0x${newValueText}`;
-      }
-
-      if (newValueText.length > 66) {
-        return false;
-      }
-
       // BigInt(...) throws if the input is invalid
       creationState.makeChange('tokenId', BigInt(newValueText));
       return true;
@@ -31,7 +18,7 @@ export const TokenIdInput = observer(() => {
   });
 
   return (
-    <Input value={tokenIdToString(creationState.tokenId)} maxLength={66} onChange={onChange}/>
+    <Input value={creationState.tokenId.toString()} onChange={onChange}/>
   );
 });
 
